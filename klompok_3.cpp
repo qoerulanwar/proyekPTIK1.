14957bac49c2410a0e3b11574be25d79f2d4fefa
kafabi af <kafabi.a.fasyah@gmail.com>
2023-12-05 08:43:54 +0700

    commit1

---
+++ klompok_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Gunung {
+    string nama;
+    double tinggi;
+    string lokasi;
+    string tipeG;
+    int tahun_letusan_terakhir;
+    int total_letusan;
+    string status;
+};
+
+int main() {
+
+  
+
+    return 0;
+}
+
